58c7a51a7d9c5b455a3c5270cc11640ba55407bb
elmonazareno <elmonazareno@gmail.com>
2019-08-14 07:58:36 +0800

    updated vehicle. fixed payment error

---
+++ client/rameses-gov-etracs-vehicle/src/com/rameses/gov/etracs/vehicle/models/VehicleDriverModel.groovy
@@ -0,0 +1,15 @@
+package com.rameses.gov.etracs.vehicle.models;
+
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.common.*;
+import com.rameses.osiris2.common.*;
+import com.rameses.osiris2.common.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.seti2.models.*;
+import com.rameses.util.*;
+
+public class VehicleDriverModel extends CrudFormModel  {
+    
+    
+    
+}
